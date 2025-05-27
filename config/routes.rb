e6e382a29c6cfa501d72954271b831ff7e8b298e
lprates05 <lorenapratesdl@outlook.com>
2025-05-27 04:22:06 +0000

    tickets show page update

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: "main#homepage"
 
   # Tickets routes
-  resources :tickets, only: [:index, :show, :new, :create, :edit, :update] do
+  resources :tickets, only: [:index, :show, :new, :create, :edit, :update, :destroy] do
   member do
       post :purchase
     end
