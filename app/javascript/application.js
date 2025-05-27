b79540bb47f4ef38a426b20d7e2f6db2f8d3eda8
lprates05 <lorenapratesdl@outlook.com>
2025-05-27 05:13:36 +0000

    delete pop-up message works

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 
-import Rails from "@rails/ujs"
+
 import jquery from "jquery";
 window.jQuery = jquery;
 window.$ = jquery;
